f0face4e6a4bf7af381dc93b414a5817cbf4431f
arch linuxer <109875196+caernarferon@users.noreply.github.com>
2023-04-16 11:21:13 -0600

    fix

--- install.py
+++ install.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
 import os
-try:
-    os.system("sudo cp rock.py ~/.local/share/rah/scripts")
 
-except (NotADirectoryError):
-    print("did you install caveman linux yet?")
-    os.system("sudo mkdir ~/.local/share/rah")
-    os.system("sudo mkdir /.local/share/rah/scripts")
+try:
+    os.system("sudo cp rah.py ~/.local/share/rah/scripts/rah")
+except NotADirectoryError:
+    print("Did you install Caveman Linux yet?")
+    os.system("sudo mkdir -p ~/.local/share/rah/scripts")
+    os.system("sudo cp rah.py ~/.local/share/rah/scripts/rah")
 finally:
-    os.system("sudo cp rock.py ~/.local/share/rah/scripts")
-os.system("sudo touch rah /usr/local/bin")
-os.system("sudo echo ")
+    os.system("sudo ln -s ~/.local/share/rah/scripts/rah ~/.local/share/rah/scripts/rah.py")
+
+# Create a symbolic link to the script in /usr/local/bin
+os.system("sudo ln -s ~/.local/share/rah/scripts/rah.py /usr/local/bin/rah")
