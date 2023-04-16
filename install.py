afce9c55f4486fb395ecf403829bb88f4311a139
arch linuxer <109875196+caernarferon@users.noreply.github.com>
2023-04-16 11:15:57 -0600

    commit

--- install.py
+++ install.py
@@ -2,12 +2,13 @@
 
 import os
 try:
-    os.system("sudo cp rock.py /opt/rok/scripts")
+    os.system("sudo cp rock.py ~/.local/share/rah/scripts")
 
 except (NotADirectoryError):
     print("did you install caveman linux yet?")
-    os.system("sudo mkdir /opt/rock/")
-    os.system("sudo mkdir /opt/rock/scripts")
-
-os.system("sudo touch /usr/local/bin")
+    os.system("sudo mkdir ~/.local/share/rah")
+    os.system("sudo mkdir /.local/share/rah/scripts")
+finally:
+    os.system("sudo cp rock.py ~/.local/share/rah/scripts")
+os.system("sudo touch rah /usr/local/bin")
 os.system("sudo echo ")
