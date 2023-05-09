ed25d4112d45949d2d905f568de96e0e10fff872
arch linuxer <109875196+caernarferon@users.noreply.github.com>
2023-05-09 12:05:24 -0600

    Update rah.py

--- rah.py
+++ rah.py
@@ -3,28 +3,44 @@ import requests
 import subprocess
 import argparse
 parse = argparse.ArgumentParser()
-parse.add_argument('-i', '--install', nargs='?', const=True, help='install a package duh')
-parse.add_argument('-r', '--remove', nargs='?', const=True, help='remove a package duh')
+parse.add_argument('-s', '--search', nargs='?', const=True, help='search for a package duh')
 args = parse.parse_args()
 
 class Rah:
     def __init__(self):
+        package = package_name
+        packagedata = 
         self.version = 0.1
         self.rahdir = os.path.expanduser("~/.rah")
-
+        self.version = package
+    def packageinfo(self, package_name):
+        package = package_name
+        self.version
+    def isitrealtho(self, package_name):
+        url = f"https://aur.archlinux.org/packages/{package_name}"
+        response = requests.get(url)
+        return response.status_code == 200
     def checkmkdir(self):
         if not os.path.isdir(self.rahdir):
             print("you dont have a rah directory, creating one...")
             os.mkdir(self.rahdir)
         else:
             return("rah directory located, moving on")
-    def install(self, packageName):
-        subprocess.run(["git", "clone", f"https://aur.archlinux.org/{packageName}.git"])
-        os.chdir(self.package)
+    def install(self, package_name):
+        os.chdir(self.rahdir)
+        print(f"changing to rah directory... {self.rahdir}")
+        try:
+            subprocess.run(["git", "clone", f"https://aur.archlinux.org/{package_name}.git"])
+        except subprocess.CalledProcessError as e:
+            if "already exists" in str(e):
+                print("i think that directory already exists. deleting...")
+            else:
+                print("uh oh: ", e)
+        os.chdir(package_name)
         os.system("makepkg -sri")
 
 rah = Rah() #capitalization
 rah.checkmkdir()
 
-package_name = input("what are you installing today: ")
+package_name = input("what are you installing today?: ")
 rah.install(package_name)
